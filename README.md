25eb8b78d58ab66ff0590c004cf74989262ffc19
João Palmeiro <joaommpalmeiro@gmail.com>
2022-08-03 19:56:42 +0100

    Add icons

--- README.md
+++ README.md
@@ -11,3 +11,6 @@
 - `npm install --save-dev vite @vitejs/plugin-react prettier prettier-config-standard`
 - `npm install react react-dom`
 - [prettier-config-standard](https://github.com/npetruzzelli/prettier-config-standard)
+- `npm install @christiankaindl/lyts` or `npm install @christiankaindl/lyts@1.2.0-beta.3` ([npm](https://www.npmjs.com/package/@christiankaindl/lyts/v/1.2.0-beta.3))
+- `npm install @jupyterlab/ui-components@3.2.5` ([npm](https://www.npmjs.com/package/@jupyterlab/ui-components/v/3.2.5))
+- [@jupyterlab/ui-components](https://github.com/jupyterlab/jupyterlab/tree/v3.2.5/packages/ui-components) (v3.2.5)
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import * as icons from '@jupyterlab/ui-components/lib/icon/iconimports'
+import { Stack, Grid, Clamp, Breakout } from '@christiankaindl/lyts'
+
+import '@christiankaindl/lyts/style.css'
+import './App.css'
+
+// https://github.com/jupyterlab/jupyterlab/blob/v3.2.5/packages/ui-components/src/icon/iconimports.ts#L91
+// Namespace import: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#namespace_import
+
+const ICON_SIZE = `${16 * 5}px`
+
 function App() {
-  return <div className="App">Hello, World!</div>
+  return (
+    <div className="App">
+      {/* https://tailwindcss.com/docs/container */}
+      <Clamp clamp="1024px" gap={ICON_SIZE}>
+        <Breakout xAlign="center">
+          <h1>@jupyterlab/ui-components@3.2.5</h1>
+        </Breakout>
+
+        <Grid gridItemMinWidth={ICON_SIZE} gap={ICON_SIZE}>
+          {Object.entries(icons).map(([iconName, icon]) => {
+            // console.log(icon)
+
+            return (
+              <Stack
+                key={icon.name}
+                xAlign="center"
+                onDoubleClick={() => console.log(iconName)}
+                style={{ cursor: 'copy' }}>
+                <img
+                  alt={icon.name}
+                  src={icon.svgstr}
+                  width={ICON_SIZE}
+                  height={ICON_SIZE}
+                />
+                <p className="truncate text-center">{iconName}</p>
+              </Stack>
+            )
+          })}
+        </Grid>
+      </Clamp>
+    </div>
+  )
 }
 
 export default App
